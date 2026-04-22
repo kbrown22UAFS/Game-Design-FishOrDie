46489ee900dcd923018481d1bb3bb18c1e340179
LuyenLe634 <lle00@uafs.edu>
2026-04-22 11:59:13 -0500

    added start button sound and script

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource clickSound;
+    public float delayBeforeLoad = 1f; // seconds
+
     public void PlayGame()
     {
+        StartCoroutine(LoadSceneAfterSound());
+    }
+
+    IEnumerator LoadSceneAfterSound()
+    {
+        clickSound.Play();
+        yield return new WaitForSeconds(delayBeforeLoad);
         SceneManager.LoadScene("FishOrDieScene");
     }
 }
